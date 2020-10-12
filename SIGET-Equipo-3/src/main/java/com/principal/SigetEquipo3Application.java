74441b31b1ae7f8c3d9dd4f6c65a6723ce31c1f8
Maria-Fatima <MFatima.Sanchez@alu.uclm.es>
2020-10-12 11:11:05 +0200

    Proyecto Spring Boot

---
+++ SIGET-Equipo-3/src/main/java/com/principal/SigetEquipo3Application.java
@@ -0,0 +1,13 @@
+package com.principal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SigetEquipo3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SigetEquipo3Application.class, args);
+	}
+
+}
