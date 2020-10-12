b54bd679aa3e9efc5ec73eb3f7535ad4af39b527
Usuario <Usuario@DESKTOP-B2T2OL9>
2020-10-12 20:11:33 +0200

    v1 con lista y eliminar, falta editar/crear

--- SIGET-Equipo-3/src/main/java/com/model/Tarea.java
+++ SIGET-Equipo-3/src/main/java/com/model/Tarea.java
@@ -1,5 +1,57 @@
 package com.model;
 
-public class Tarea {
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
+@Document(collection = "tareas")
+public class Tarea {
+	
+	public ObjectId getDatabaseId() {
+		return databaseId;
+	}
+
+	public void setDatabaseId(ObjectId databaseId) {
+		this.databaseId = databaseId;
+	}
+
+	@Id private ObjectId databaseId;
+	private int id;
+	
+	private String descripcion;
+	private boolean acabada;
+	
+	public Tarea() {}
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	public boolean isAcabada() {
+		return acabada;
+	}
+
+	public void setAcabada(boolean acabada) {
+		this.acabada = acabada;
+	}
+
+	@Override
+	public String toString() {
+		return "Tarea [id=" + id + ", descripcion=" + descripcion + ", acabada=" + acabada + "]";
+	}
+	
+	
+	
 }
