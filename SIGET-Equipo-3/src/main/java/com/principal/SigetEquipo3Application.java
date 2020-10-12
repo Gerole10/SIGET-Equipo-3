9587f48761d576b3e379d7ca1b01e3afb2820be4
Maria-Fatima <MFatima.Sanchez@alu.uclm.es>
2020-10-12 11:18:16 +0200

    Prueba

--- SIGET-Equipo-3/src/main/java/com/principal/SigetEquipo3Application.java
+++ SIGET-Equipo-3/src/main/java/com/principal/SigetEquipo3Application.java
@@ -7,8 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SigetEquipo3Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SigetEquipo3Application.class, args);
-		
+		SpringApplication.run(SigetEquipo3Application.class, args);	
 	}
-
 }
